d88e52f01bf714ff18879bc546edfeb815e49f45
yuukidach <chendamailbox@foxmail.com>
2021-03-14 12:14:31 +0800

    feat: add new activity

---
+++ app/src/main/java/hk/hkucs/workshop2/CourseListActivity.java
@@ -0,0 +1,14 @@
+package hk.hkucs.workshop2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.course_list_item);
+    }
+}
